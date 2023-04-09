1290198fa716fddd9881333b9e39d4d1ef9dc662
AlbertBarreiro <albert.barreiro@estudiantat.upc.edu>
2023-04-09 18:34:42 +0200

    plot optical flow

--- w4/main.py
+++ w4/main.py
@@ -14,7 +14,7 @@ from block_matching import compute_block_matching
 
 ERROR_THRESH = 3
 
-from plot_data import plot_3D
+from plot_data import plot_3D, plot_optical_flow
 
 def task11(gridSearch=True, distance='cv2.TM_CCORR_NORMED'):
     # distance = 'cv2.TM_SQDIFF_NORMED'
@@ -27,7 +27,7 @@ def task11(gridSearch=True, distance='cv2.TM_CCORR_NORMED'):
     if gridSearch:
         # motion = ['forward','backward']# uncomment when not using the 3d plot
         motion = ['backward']
-        blockSize = [4, 8, 16, 32, 64]
+        blockSize = [4]
         searchAreas = np.array(blockSize) * 2 + np.array(blockSize)
 
     else:
@@ -43,7 +43,8 @@ def task11(gridSearch=True, distance='cv2.TM_CCORR_NORMED'):
     end_time = []
     pepns = []
     msens = []
-
+    plot_metrics = True
+    best_predicted_flow = None
     for m in motion:
         for i, bs in enumerate(blockSize):
             # quantStep = [int(bs/2),bs]
@@ -59,8 +60,8 @@ def task11(gridSearch=True, distance='cv2.TM_CCORR_NORMED'):
                     #     plotOf.plotArrowsOP(predicted_flow, 10, im2)
                     # else:
                     #      plotOf.plotArrowsOP(predicted_flow, 10, im1)
-                    msen = calculate_msen(predicted_flow, flow_gt, th=ERROR_THRESH)
-                    pepn = calculate_pepn(predicted_flow, flow_gt, th=ERROR_THRESH)
+                    msen = calculate_msen(flow_gt, predicted_flow, th=ERROR_THRESH)
+                    pepn = calculate_pepn(flow_gt, predicted_flow, th=ERROR_THRESH)
                     pepns.append(pepn)
                     msens.append(msen)
 
@@ -74,6 +75,7 @@ def task11(gridSearch=True, distance='cv2.TM_CCORR_NORMED'):
                     print('pepn: ', pepn)
                     errsum = msen + pepn
                     if errsum < minerr:
+                        best_predicted_flow = predicted_flow
                         bestQ = q
                         minerr = errsum
                         bestMotion = m
@@ -84,9 +86,11 @@ def task11(gridSearch=True, distance='cv2.TM_CCORR_NORMED'):
                     end = time.time()
                     all_time[i,j] = end - start
                 end_time.append(time.time())
-    #plot_3D(blockSize, searchAreas, all_msen, 'Block Size', 'Search Areas', 'MSEN')
-    #plot_3D(blockSize, searchAreas, all_pepn, 'Block Size', 'Search Areas', 'PEPN')
-    #plot_3D(blockSize, searchAreas, all_time, 'Block Size', 'Search Areas', 'Execution time')
+    if plot_metrics:
+        plot_optical_flow(im2, best_predicted_flow)
+        plot_3D(blockSize, searchAreas, all_msen, 'Block Size', 'Search Areas', 'MSEN')
+        plot_3D(blockSize, searchAreas, all_pepn, 'Block Size', 'Search Areas', 'PEPN')
+        plot_3D(blockSize, searchAreas, all_time, 'Block Size', 'Search Areas', 'Execution time')
     if gridSearch:
         print('Best M', bestMotion)
         print('Best BS', bestBlock)
